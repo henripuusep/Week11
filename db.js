86d86fffbb7404579ceaef5960ccb2416f045899
Henri Puusep <henripuusep12@gmail.com>
2023-12-11 19:59:12 +0200

    week 11 dotenv

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const { Pool } = require('pg');
+
 const itemsPool = new Pool({
-    user: 'postgres',
-    password: 'admin1234',
-    host: 'localhost',
-    port: 5432,
-    database: 'myrecipes'
+    connectionString: process.env.DBConnectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
 
 module.exports = itemsPool;
